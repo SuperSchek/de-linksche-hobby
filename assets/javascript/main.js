a03aadc5adc26687be52e755bf0db259cfa77a7b
Sander Schekman <sanderschekman@gmail.com>
2018-07-15 20:36:29 +0200

    Adds cookiebar

---
+++ assets/javascript/main.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+    const cookieBtn = document.getElementById('cookieBtn');
+    cookieBtn.onclick = () => {
+        document.getElementById('cookieBar').className = 'cookiebar closed';
+    };
+    
+});
